cf5c311f319db9aab962ee91e59ae123858526ac
Bartosz <bart.oleaczek@gmail.com>
2020-01-21 22:22:41 +0100

    prepared tests cases for movement_manager

--- src/game_controller/movement_manager.rs
+++ src/game_controller/movement_manager.rs
@@ -1,6 +1,20 @@
-use crate::algebra_basics::{Coordinates, Size};
-use crate::game_data::gameboard::GameboardObjectOperation;
+use crate::algebra_basics::{Coordinates, Size, Vector};
 
 pub struct MovementHandler {
 
+}
+
+#[cfg(test)]
+mod tests {
+use super::*;
+
+    #[test]
+    fn movement_handler_poll_position_changes_correctly() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn movement_handler_poll_returns_end_when_destination_is_reached() {
+        unimplemented!();
+    }
 }
