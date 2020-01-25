ca981232681503e98cbadb8a169e4b65376b7449
Bartosz <bart.oleaczek@gmail.com>
2020-01-25 13:29:54 +0100

    prepared pathfinding and basic uts

--- src/game_controller/movement_manager.rs
+++ src/game_controller/movement_manager.rs
@@ -1,4 +1,6 @@
-use crate::algebra_basics::{Coordinates, Size, Vector};
+use crate::algebra_basics::{Coordinates, Size, Vector, LineEquation};
+use crate::game_data::gameboard::Gameboard;
+use crate::game_data::game_object::{GameObject, GameObjectType};
 
 pub struct MovementHandler {
     path: Vec<Coordinates>,
@@ -46,6 +48,19 @@ impl MovementHandler {
     }
 }
 
+pub fn find_path(start: &Coordinates, destination: &Coordinates, gameboard: &Gameboard) -> Vec<Coordinates> {
+    let game_objects = gameboard.get_all_objects();
+    let line_equation = LineEquation::get_line_equation(start, destination);
+    let mut points = Vec::new();
+
+    for object in game_objects {
+        
+    }
+
+    points.push(destination.clone());
+    points
+}
+
 #[cfg(test)]
 mod tests {
 use super::*;
@@ -77,5 +92,41 @@ use super::*;
 
         assert_eq!(movement_handler.poll_movement().is_some(), true);
         assert_eq!(movement_handler.poll_movement().is_none(), true);
+    }
+
+    fn setup_gameboard_with_obstacle(obstacle_coordinates: Coordinates) -> Gameboard {
+        let mut gameboard = Gameboard::new();
+        let obstacle = GameObject::new(GameObjectType::Static, obstacle_coordinates, Size::new(100.0, 100.0));
+        gameboard.add_object(obstacle);
+
+        return gameboard;
+    }
+
+    #[test]
+    fn find_path_obstacle_in_front_of_start_on_x_axis_correct_path_with_3_points_is_calculated() {
+        let gameboard = setup_gameboard_with_obstacle(Coordinates::new(150.0, 100.0));
+        let path = find_path(&Coordinates::new(100.0, 100.0), &Coordinates::new(200.0, 100.0), &gameboard);
+        assert_eq!(path.len(), 3); 
+    }
+
+    #[test]
+    fn find_path_obstacle_behind_start_on_x_axis_correct_path_with_1_point_is_calculated() {
+        let gameboard = setup_gameboard_with_obstacle(Coordinates::new(100.0, 100.0));
+        let path = find_path(&Coordinates::new(150.0, 100.0), &Coordinates::new(200.0, 100.0), &gameboard);
+        assert_eq!(path.len(), 1);
+    }
+
+    #[test]
+    fn find_path_obstacle_in_front_of_start_on_y_axis_correct_path_with_3_points_is_calculated() {
+        let gameboard = setup_gameboard_with_obstacle(Coordinates::new(100.0, 150.0));
+        let path = find_path(&Coordinates::new(100.0, 100.0), &Coordinates::new(100.0, 200.0), &gameboard);
+        assert_eq!(path.len(), 3);
+    }
+
+    #[test]
+    fn find_path_obstacle_behind_start_on_y_axis_correct_path_with_1_point_is_calculated() {
+        let gameboard = setup_gameboard_with_obstacle(Coordinates::new(100.0, 100.0));
+        let path = find_path(&Coordinates::new(100.0, 150.0), &Coordinates::new(100.0, 200.0), &gameboard);
+        assert_eq!(path.len(), 1);
     }
 }
